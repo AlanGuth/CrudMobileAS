7c0c1fc9d913b8974ab1e8ece6f8c84b7ec0a65a
Alan Sergio Guth <alan_guth@hotmail.com>
2021-01-29 13:15:38 -0300

    **COMMIT INICIAL**

---
+++ CrudMobileAS/app/src/main/java/com/example/crudmobileas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.crudmobileas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
